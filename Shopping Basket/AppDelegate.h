109728ee4f8b6394567c89f8c89d61093630ea7b
Etienne la Grange <etienne.la.grange@multilateral.com>
2016-05-31 08:15:46 +0200

    Initial Commit

---
+++ Shopping Basket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shopping Basket
+//
+//  Created by Etienne La Grange on 5/31/16.
+//  Copyright © 2016 Etienne la Grange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shopping Basket/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Shopping Basket
+//
+//  Created by Etienne La Grange on 5/31/16.
+//  Copyright © 2016 Etienne la Grange. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
